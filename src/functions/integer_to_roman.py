c04605715695092c1d852215e65b3d6d402074b4
Sergey <herol.akkusativ@mail.ru>
2023-12-27 11:53:27 +0300

    feat: add integer_to_roman function

---
+++ src/functions/integer_to_roman.py
@@ -0,0 +1,32 @@
+def int_to_roman(num: int):
+    result = str()
+
+    roman_nums = {
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I"
+    }
+    for roman in roman_nums:
+        quotient = num // roman
+        result += roman_nums[roman] * quotient
+        num -= roman * quotient
+
+    return result
+
+
+if __name__ == '__main__':
+    print(int_to_roman(100000), "XXXII")
+    print(int_to_roman(33), "XXXIII")
+    print(int_to_roman(34), "XXXIV")
+    print(int_to_roman(54), "LIV")
+    print(int_to_roman(1984), "MCMLXXXIV")
