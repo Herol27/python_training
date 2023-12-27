40e71221c8a5747de7d468a2546379456c4b2a53
Sergey <herol.akkusativ@mail.ru>
2023-12-27 11:53:49 +0300

    feat: add integer_to_roman to my_api

--- src/my_api.py
+++ src/my_api.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 
 from functions.csv_to_dict_converter import convert_csv_to_dict
 from functions.different_registers_finder import find_in_different_registers
+from functions.integer_to_roman import int_to_roman
 
 
 class WordList(BaseModel):
@@ -34,6 +35,15 @@ def calculate_average_age_by_position(response: Response, csv_file: UploadFile =
 @app.post("/find_in_different_registers")
 def delete_duplicates_in_different_registers(word_list: WordList):
     return find_in_different_registers(word_list.words)
+
+
+@app.post("/int_to_roman")
+def calculate_int_to_roman(num: int, response: Response):
+    if 0 <= num <= 100000:
+        return int_to_roman(num)
+    else:
+        response.status_code = fastapi.status.HTTP_400_BAD_REQUEST
+        return "Некорректное число. Число должно лежать в промежутке [0, 10000]"
 
 
 if __name__ == '__main__':
